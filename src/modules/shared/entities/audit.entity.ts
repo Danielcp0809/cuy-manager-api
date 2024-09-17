4ebcb43f8beb576b5722566869daa3b29921498a
Daniel Cordova <dcordova@udtonline.com>
2024-09-16 22:00:07 -0500

    [Add] audit entity and shared module

---
+++ src/modules/shared/entities/audit.entity.ts
@@ -0,0 +1,16 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Audit extends BaseEntity {
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
+}
