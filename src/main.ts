26227a3bcfae274d670365675cb188607dea364f
Daniel Cordova <dcordova@udtonline.com>
2024-09-16 21:53:14 -0500

    [Add] Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Cuy Manager API')
+    .setDescription(
+      'Platform for managing guinea pig farms, tracking animals by category, optimizing resources, and generating detailed reports',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // Enable validations globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true, // Convert query params to their respective types
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  await app.listen(process.env.APP_PORT || 3030);
 }
 bootstrap();
