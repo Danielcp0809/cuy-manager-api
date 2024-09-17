74dfeecbc4f4795d11649ee2e634f3f52caaef98
Daniel Cordova <dcordova@udtonline.com>
2024-09-16 22:58:18 -0500

    [Add] Payload exits pipe

---
+++ src/pipes/payload-exists/payload-exists.pipe.ts
@@ -0,0 +1,14 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+Injectable();
+export class ValidatePayloadExistsPipe implements PipeTransform {
+  transform(payload: any): any {
+    if (!Object.keys(payload).length) {
+      throw new BadRequestException(
+        'Payload should have al least one property',
+      );
+    }
+
+    return payload;
+  }
+}
