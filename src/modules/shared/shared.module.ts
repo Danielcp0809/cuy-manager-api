f3a60f2ea81c4af5a8c049783b8374d5d8a4338f
Daniel Cordova <dcordova@udtonline.com>
2024-09-16 23:38:05 -0500

    [Add] Typeorm configuration in shared module

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from 'src/config/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+  ],
+  providers: [],
+  exports: [TypeOrmModule],
+})
 export class SharedModule {}
