f460b1aa794a4df97a368687893b55060d87006c
Daniel Cordova <dcordova@udtonline.com>
2024-09-17 00:29:12 -0500

    [Add] Create credentials method

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Credentials } from 'src/models/credentials.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Credentials])],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Credentials } from 'src/models/credentials.entity';
+import { Enterprise } from 'src/models/enterprises.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(Credentials)
+    private credentialsRepository: Repository<Credentials>,
+  ) {}
+
+  async createCredentials(enterprise: Enterprise, email: string) {
+    const password = Math.random().toString(36).slice(2, 12);
+    const encryptedPassword = await this.encryptPassword(password);
+    const newCredentials = new Credentials();
+    newCredentials.username = email;
+    newCredentials.password = encryptedPassword;
+    newCredentials.enterprise = enterprise;
+    return {
+      credentials: await this.credentialsRepository.save(newCredentials),
+      password,
+    };
+  }
+
+  async encryptPassword(password: string) {
+    return await bcrypt.hash(password, 10);
+  }
+}
