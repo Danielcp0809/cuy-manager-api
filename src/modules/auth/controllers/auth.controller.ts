db697c5eb7116c102a08c2dd072c0bb9810f0686
Daniel Cordova <jdany0809@hotmail.com>
2024-10-14 01:15:15 -0500

    [v1.1.0] Forgot password feature

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,5 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { CredentialsDto, RefreshTokenDto } from 'src/validators/auth.dto';
+import {
+  CredentialsDto,
+  ForgotPasswordDto,
+  RefreshTokenDto,
+  ResetPasswordDto,
+  VerifyCodeDto,
+} from 'src/validators/auth.dto';
 import { AuthService } from '../services/auth.service';
 @Controller('auth')
 export class AuthController {
@@ -15,5 +21,27 @@ export class AuthController {
   @Post('/refresh')
   refreshToken(@Body() body: RefreshTokenDto) {
     return this.authService.refreshToken(body);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/forgot-password')
+  forgotPassword(@Body() body: ForgotPasswordDto) {
+    return this.authService.forgotPassword(body.email);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/verify-code')
+  verifyCode(@Body() body: VerifyCodeDto) {
+    return this.authService.verifyCode(body.email, body.code);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/reset-password')
+  resetPassword(@Body() body: ResetPasswordDto) {
+    return this.authService.changePassword(
+      body.email,
+      body.password,
+      body.code,
+    );
   }
 }
